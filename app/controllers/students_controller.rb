3b38c5da6f3563655daaa15804bd910264f935c2
mplichta <bigcatplichta@gmail.com>
2019-11-06 15:54:43 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def create
+    session[:form_params] = params.inspect
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    # use permit! for mass assignment above?
+    redirect_to new_student_path
+  end
+
+  def new
+  end
+
 end
